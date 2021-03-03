ab2b6d2b50bc310ccacf2faa716c800a38808b1e
Chathray <48251215+Chathray@users.noreply.github.com>
2021-03-03 09:11:35 +0700

    No.8

---
+++ Calculator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loop = true;
+            float result = 0;
+
+            while (loop)
+            {
+            begin:
+                Console.Write("\nPress +, -, x or : to set your operator: ");
+                char operato = Console.ReadKey().KeyChar;
+
+                Console.Write("\nEnter a number: ");
+                int n1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter another number: ");
+                int n2 = Convert.ToInt32(Console.ReadLine());
+
+                switch (operato)
+                {
+                    case '+':
+                        result = n1 + n2;
+                        break;
+                    case '-':
+                        result = n1 - n2;
+                        break;
+                    case 'x':
+                        result = n1 * n2;
+                        break;
+                    case ':':
+                        result = n1 / n2;
+                        break;
+                    default:
+                        Console.WriteLine("The operator is not supported!");
+                        break;
+                }
+                Console.Write("{0} {3} {1} = {2}\n", n1, n2, result, operato);
+
+                Console.WriteLine("Press Enter to continues...\n");
+                loop = Console.ReadKey().KeyChar == 13;
+            }
+        }
+    }
+}
