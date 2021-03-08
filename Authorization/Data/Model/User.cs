ec6b972104929fc72ecc528561d43d768ad3c7c0
Chathray <48251215+Chathray@users.noreply.github.com>
2021-03-08 09:46:43 +0700

    5/3/2021

---
+++ Authorization/Data/Model/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication
+{
+    public class User
+    {
+        [Key]
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string PasswordHash { get; set; }
+    }
+}
